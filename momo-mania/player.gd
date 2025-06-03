42a51dd44e090d0ca844e00073525cbfb0e833c6
Shreenish Kumar Shakya <shreenishshakya025@gmail.com>
2025-06-03 19:35:42 +0545

    d=fixed addition of movement
    i added and  fixed the gd script for the character and added a temporary camera for now.

--- momo-mania/player.gd
+++ momo-mania/player.gd
@@ -1,36 +1,31 @@
 extends CharacterBody3D
-#character speed
-@export var speed = 14
-#downward acceleration
-@export var fall_acceleration = 75
 
-var target_velocity = Vector3.ZERO
+@export var speed := 4.0
+@export var gravity := 4.0
+@export var jump_force := 7.0
+
+@onready var player_body = $"Pivot/momo dai"
+var angular_speed = 10
+
+
+
+var movement
+var direction
 
 func _physics_process(delta):
-	var direction = Vector3.ZERO
+	move(delta)
+
+func move(delta):
+	movement = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	direction = (transform.basis * Vector3(movement.x, 0, movement.y)).normalized()
 	
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	
-	if Input.is_action_just_pressed("move_left"):
-		direction.x -= 1
-	
-	if Input.is_action_just_pressed("move_back"):
-		direction.z += 1
-	
-	if Input.is_action_just_pressed("move_forward"):
-		direction.z -= 1
-	
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+		player_body.rotation.y = lerp_angle(player_body.rotation.y, atan2(velocity.x, velocity.z), delta * angular_speed)
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 		
-		$Pivot.basis = Basis.looking_at(direction)
-		
-	#ground velocity
-	target_velocity = direction.x * speed
-	target_velocity = direction.z * speed
+	move_and_slide()
 	
-	#vertical velocity
-	if not is_on_floor():
-		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
-		move_and_slide()
